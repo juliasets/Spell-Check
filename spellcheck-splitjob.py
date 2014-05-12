c98393c8454df55b8a0b437fc8ba35bea0babbba
Collin RM Stocks <collinstocks@gmail.com>
2014-05-12 17:58:48 -0400

    bug fix, unexpected newline

--- spellcheck-splitjob.py
+++ spellcheck-splitjob.py
@@ -56,7 +56,7 @@ proc = subprocess.Popen(
     stdin=subprocess.PIPE)
 proc.communicate(%s)
 if proc.returncode != 0:
-    sys.stderr.write('Child died with error code ' + \
+    sys.stderr.write('Child died with error code ' + \\
         str(proc.returncode) + '\n')
 exit(proc.returncode)
 """ % ('../Dictionary/unigrams.txt', '../Dictionary/trained21.txt', '../Dictionary/BigramDatabase.db', repr(data))
